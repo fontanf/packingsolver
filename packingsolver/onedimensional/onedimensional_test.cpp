c8b6bb8698ac3836c6d281b960f18930c1c83a91
Florian Fontan <dev@florian-fontan.fr>
2024-04-18 23:56:22 +0200

    Add test for onedimensional

--- packingsolver/onedimensional/onedimensional_test.cpp
+++ packingsolver/onedimensional/onedimensional_test.cpp
@@ -19,3 +19,19 @@ TEST(OneDimensional, BinCopies)
     EXPECT_EQ(solution.number_of_bins(), 2);
     EXPECT_EQ(solution.bin_copies(0), 2);
 }
+
+TEST(OneDimensional, Users20240409)
+{
+    InstanceBuilder instance_builder;
+    instance_builder.set_objective(packingsolver::Objective::BinPackingWithLeftovers);
+    fs::path directory = fs::path("data") / "onedimensional" / "users";
+    instance_builder.read_item_types((directory / "2024-04-09_items.csv").string());
+    instance_builder.read_bin_types((directory / "2024-04-09_bins.csv").string());
+    Instance instance = instance_builder.build();
+
+    OptimizeParameters optimize_parameters;
+    optimize_parameters.optimization_mode = packingsolver::OptimizationMode::NotAnytime;
+    Output output = optimize(instance, optimize_parameters);
+
+    EXPECT_EQ(output.solution_pool.best().cost(), 27800);
+}
