197b78a79dad66b75503d316a15ee56f440b2052
Florian Fontan <dev@florian-fontan.fr>
2024-05-12 22:51:53 +0200

    Set column generation dummy columns coefficient (#25)

--- src/onedimensional/optimize.cpp
+++ src/onedimensional/optimize.cpp
@@ -285,6 +285,7 @@ void optimize_column_generation(
     cgslds_parameters.verbosity_level = 0;
     cgslds_parameters.timer = parameters.timer;
     cgslds_parameters.internal_diving = 1;
+    cgslds_parameters.dummy_column_objective_coefficient = 2 * instance.bin_type(0).cost;
     if (parameters.optimization_mode != OptimizationMode::Anytime)
         cgslds_parameters.automatic_stop = true;
     cgslds_parameters.new_solution_callback = [&instance, &algorithm_formatter](
