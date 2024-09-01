051462e16fb29a5d226a5de4ec815e3fc4552a10
Florian Fontan <dev@florian-fontan.fr>
2024-09-01 22:27:56 +0200

    Integrate CDT dependency

---
+++ src/irregular/polygon_simplification.cpp
@@ -0,0 +1,12 @@
+#include "irregular/polygon_simplification.hpp"
+
+#include "CDT.h"
+
+using namespace packingsolver;
+using namespace packingsolver::irregular;
+
+Shape irregular::polygon_simplification(
+        const Shape& shape)
+{
+    return shape;
+}
