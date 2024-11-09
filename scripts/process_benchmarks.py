7e261fbfcfa079daa4d06f5f5e228343d71ca1db
Florian Fontan <dev@florian-fontan.fr>
2024-11-09 09:17:48 +0100

    Update benchmark script

--- scripts/process_benchmarks.py
+++ scripts/process_benchmarks.py
@@ -7,7 +7,14 @@ import datetime
 import pandas as pd
 
 
+st.set_page_config(layout='wide')
 st.title("PackingSolver benchmarks analyzer")
+
+def show_datafram(df):
+    st.dataframe(
+            df,
+            use_container_width=True,
+            height=(len(df.index) + 1) * 35 + 3)
 
 benchmarks = [
     f
@@ -31,8 +38,93 @@ bksv_field = "Best known solution value"
 
 
 if benchmark == "rectangleguillotine_roadef2018":
-    # TODO
-    pass
+
+    datacsv_path = os.path.join(
+            "data",
+            "rectangle",
+            "data_roadef2018.csv")
+
+    data_dir = os.path.dirname(os.path.realpath(datacsv_path))
+    with open(datacsv_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        # Get fieldnames of CSV output file.
+        out_fieldnames = reader.fieldnames
+        for output_directory in output_directories:
+            out_fieldnames.append(output_directory)
+
+        result_columns = ["parreno2020 (3600s)", "libralesso2021"]
+
+        out_rows = []
+        # Initialize extra rows.
+        extra_rows = [{"Path": p} for p in ["a", "b", "x", "b+x", "a+b+x"]]
+        for result_column in [bksv_field] + result_columns + output_directories:
+            for row in extra_rows:
+                row[result_column] = 0
+
+        for row in reader:
+            row[bksv_field] = int(row[bksv_field])
+
+            # Get extra rows to update.
+            extra_rows_to_update = []
+            if row["Dataset"] == "a":
+                extra_rows_to_update.append(0)
+            elif row["Dataset"] == "b":
+                extra_rows_to_update.append(1)
+                extra_rows_to_update.append(3)
+            elif row["Dataset"] == "x":
+                extra_rows_to_update.append(2)
+                extra_rows_to_update.append(3)
+            extra_rows_to_update.append(4)
+
+            # Update "Best known solution value" column.
+            for row_id in extra_rows_to_update:
+                extra_rows[row_id][bksv_field] += int(row[bksv_field])
+
+            for output_directory in output_directories:
+                # Retrieve value.
+                json_output_path = os.path.join(
+                        benchmark_directory,
+                        output_directory,
+                        row["Path"] + "_output.json")
+                json_output_file = open(json_output_path, "r")
+                json_data = json.load(json_output_file)
+                waste = int(json_data["Output"]
+                                     ["Solution"]
+                                     ["Waste"])
+                # Update current row.
+                row[output_directory] = waste
+                # Update extra rows.
+                for row_id in extra_rows_to_update:
+                    extra_rows[row_id][output_directory] += waste
+
+            # Update result columns of extra rows.
+            for result_column in result_columns:
+                waste = int(row[result_column])
+                row[result_column] = waste
+                for row_id in extra_rows_to_update:
+                    extra_rows[row_id][result_column] += waste
+
+            # Add current row.
+            out_rows.append(row)
+
+        # Add extra rows.
+        for row in extra_rows:
+            out_rows.append(row)
+
+        df = pd.DataFrame.from_records(out_rows, columns=out_fieldnames)
+
+        def highlight(s):
+            return [('background-color: lightgreen'
+                     if s[fieldname] == s[bksv_field]
+                     else ('background-color: pink'
+                           if s[fieldname] > s[bksv_field]
+                           else 'background-color: yellow'))
+                    if fieldname in result_columns + output_directories
+                    else ''
+                    for fieldname in out_fieldnames]
+        df = df.style.apply(highlight, axis = 1)
+        show_datafram(df)
 
 
 elif benchmark == "rectangleguillotine_bin_packing_3nho":
@@ -51,7 +143,7 @@ elif benchmark == "rectangleguillotine_bin_packing_3nho":
         for output_directory in output_directories:
             out_fieldnames.append(output_directory)
 
-        last_rows = [
+        extra_rows = [
                 {
                     "Path": ("Class_" + instance_class
                               + ".2bp_" + number_of_items),
@@ -63,13 +155,13 @@ elif benchmark == "rectangleguillotine_bin_packing_3nho":
             ]
         for output_directory in output_directories:
             for i in range(50):
-                last_rows[i][output_directory] = 0
+                extra_rows[i][output_directory] = 0
 
-        last_row = {
+        extra_row = {
                 "Path": "Total",
                 bksv_field: 0}
         for output_directory in output_directories:
-            last_row[output_directory] = 0
+            extra_row[output_directory] = 0
 
         for row in reader:
             if not row["Path"]:
@@ -81,8 +173,8 @@ elif benchmark == "rectangleguillotine_bin_packing_3nho":
             row_id = (
                     (instance_class - 1) * 5
                     + int(number_of_items / 20 - 1))
-            last_row[bksv_field] += int(row[bksv_field])
-            last_rows[row_id][bksv_field] += int(row[bksv_field])
+            extra_row[bksv_field] += int(row[bksv_field])
+            extra_rows[row_id][bksv_field] += int(row[bksv_field])
 
             for output_directory in output_directories:
                 json_output_path = os.path.join(
@@ -95,26 +187,26 @@ elif benchmark == "rectangleguillotine_bin_packing_3nho":
                                               ["Solution"]
                                               ["NumberOfBins"])
                 row[output_directory] = number_of_bins
-                last_rows[row_id][output_directory] += number_of_bins
-                last_row[output_directory] += number_of_bins
+                extra_rows[row_id][output_directory] += number_of_bins
+                extra_row[output_directory] += number_of_bins
 
             out_rows.append(row)
 
         for i in range(50):
-            out_rows.append(last_rows[i])
-        out_rows.append(last_row)
+            out_rows.append(extra_rows[i])
+        out_rows.append(extra_row)
 
         df = pd.DataFrame.from_records(out_rows, columns=out_fieldnames)
 
         def highlight(s):
-            return [('background-color: green'
+            return [('background-color: lightgreen'
                      if s[fieldname] == int(s[bksv_field])
-                     else 'background-color: red')
+                     else 'background-color: pink')
                     if fieldname in output_directories
                     else ''
                     for fieldname in out_fieldnames]
         df = df.style.apply(highlight, axis = 1)
-        st.dataframe(df)
+        show_datafram(df)
 
 
 elif benchmark == "rectangleguillotine_bin_packing_3nhr":
@@ -133,7 +225,7 @@ elif benchmark == "rectangleguillotine_bin_packing_3nhr":
         for output_directory in output_directories:
             out_fieldnames.append(output_directory)
 
-        last_rows = [
+        extra_rows = [
                 {
                     "Path": ("Class_" + instance_class
                               + ".2bp_" + number_of_items),
@@ -145,13 +237,13 @@ elif benchmark == "rectangleguillotine_bin_packing_3nhr":
             ]
         for output_directory in output_directories:
             for i in range(50):
-                last_rows[i][output_directory] = 0
+                extra_rows[i][output_directory] = 0
 
-        last_row = {
+        extra_row = {
                 "Path": "Total",
                 bksv_field: 0}
         for output_directory in output_directories:
-            last_row[output_directory] = 0
+            extra_row[output_directory] = 0
 
         for row in reader:
             if not row["Path"]:
@@ -163,8 +255,8 @@ elif benchmark == "rectangleguillotine_bin_packing_3nhr":
             row_id = (
                     (instance_class - 1) * 5
                     + int(number_of_items / 20 - 1))
-            last_row[bksv_field] += int(row[bksv_field])
-            last_rows[row_id][bksv_field] += int(row[bksv_field])
+            extra_row[bksv_field] += int(row[bksv_field])
+            extra_rows[row_id][bksv_field] += int(row[bksv_field])
 
             for output_directory in output_directories:
                 json_output_path = os.path.join(
@@ -177,26 +269,26 @@ elif benchmark == "rectangleguillotine_bin_packing_3nhr":
                                               ["Solution"]
                                               ["NumberOfBins"])
                 row[output_directory] = number_of_bins
-                last_rows[row_id][output_directory] += number_of_bins
-                last_row[output_directory] += number_of_bins
+                extra_rows[row_id][output_directory] += number_of_bins
+                extra_row[output_directory] += number_of_bins
 
             out_rows.append(row)
 
         for i in range(50):
-            out_rows.append(last_rows[i])
-        out_rows.append(last_row)
+            out_rows.append(extra_rows[i])
+        out_rows.append(extra_row)
 
         df = pd.DataFrame.from_records(out_rows, columns=out_fieldnames)
 
         def highlight(s):
-            return [('background-color: green'
+            return [('background-color: lightgreen'
                      if s[fieldname] == int(s[bksv_field])
-                     else 'background-color: red')
+                     else 'background-color: pink')
                     if fieldname in output_directories
                     else ''
                     for fieldname in out_fieldnames]
         df = df.style.apply(highlight, axis = 1)
-        st.dataframe(df)
+        show_datafram(df)
 
 
 elif benchmark == "rectangle_bin_packing_oriented":
@@ -215,7 +307,7 @@ elif benchmark == "rectangle_bin_packing_oriented":
         for output_directory in output_directories:
             out_fieldnames.append(output_directory)
 
-        last_rows = [
+        extra_rows = [
                 {
                     "Path": ("Class_" + instance_class
                               + ".2bp_" + number_of_items),
@@ -227,13 +319,13 @@ elif benchmark == "rectangle_bin_packing_oriented":
             ]
         for output_directory in output_directories:
             for i in range(50):
-                last_rows[i][output_directory] = 0
+                extra_rows[i][output_directory] = 0
 
-        last_row = {
+        extra_row = {
                 "Path": "Total",
                 bksv_field: 0}
         for output_directory in output_directories:
-            last_row[output_directory] = 0
+            extra_row[output_directory] = 0
 
         for row in reader:
             if not row["Path"]:
@@ -245,8 +337,8 @@ elif benchmark == "rectangle_bin_packing_oriented":
             row_id = (
                     (instance_class - 1) * 5
                     + int(number_of_items / 20 - 1))
-            last_row[bksv_field] += int(row[bksv_field])
-            last_rows[row_id][bksv_field] += int(row[bksv_field])
+            extra_row[bksv_field] += int(row[bksv_field])
+            extra_rows[row_id][bksv_field] += int(row[bksv_field])
 
             for output_directory in output_directories:
                 json_output_path = os.path.join(
@@ -259,26 +351,26 @@ elif benchmark == "rectangle_bin_packing_oriented":
                                               ["Solution"]
                                               ["NumberOfBins"])
                 row[output_directory] = number_of_bins
-                last_rows[row_id][output_directory] += number_of_bins
-                last_row[output_directory] += number_of_bins
+                extra_rows[row_id][output_directory] += number_of_bins
+                extra_row[output_directory] += number_of_bins
 
             out_rows.append(row)
 
         for i in range(50):
-            out_rows.append(last_rows[i])
-        out_rows.append(last_row)
+            out_rows.append(extra_rows[i])
+        out_rows.append(extra_row)
 
         df = pd.DataFrame.from_records(out_rows, columns=out_fieldnames)
 
         def highlight(s):
-            return [('background-color: green'
+            return [('background-color: lightgreen'
                      if s[fieldname] == int(s[bksv_field])
-                     else 'background-color: red')
+                     else 'background-color: pink')
                     if fieldname in output_directories
                     else ''
                     for fieldname in out_fieldnames]
         df = df.style.apply(highlight, axis = 1)
-        st.dataframe(df)
+        show_datafram(df)
 
 
 elif benchmark == "rectangle_bin_packing_rotation":
@@ -297,7 +389,7 @@ elif benchmark == "rectangle_bin_packing_rotation":
         for output_directory in output_directories:
             out_fieldnames.append(output_directory)
 
-        last_rows = [
+        extra_rows = [
                 {
                     "Path": ("Class_" + instance_class
                               + ".2bp_" + number_of_items),
@@ -309,13 +401,13 @@ elif benchmark == "rectangle_bin_packing_rotation":
             ]
         for output_directory in output_directories:
             for i in range(50):
-                last_rows[i][output_directory] = 0
+                extra_rows[i][output_directory] = 0
 
-        last_row = {
+        extra_row = {
                 "Path": "Total",
                 bksv_field: 0}
         for output_directory in output_directories:
-            last_row[output_directory] = 0
+            extra_row[output_directory] = 0
 
         for row in reader:
             if not row["Path"]:
@@ -327,8 +419,8 @@ elif benchmark == "rectangle_bin_packing_rotation":
             row_id = (
                     (instance_class - 1) * 5
                     + int(number_of_items / 20 - 1))
-            last_row[bksv_field] += int(row[bksv_field])
-            last_rows[row_id][bksv_field] += int(row[bksv_field])
+            extra_row[bksv_field] += int(row[bksv_field])
+            extra_rows[row_id][bksv_field] += int(row[bksv_field])
 
             for output_directory in output_directories:
                 json_output_path = os.path.join(
@@ -341,23 +433,23 @@ elif benchmark == "rectangle_bin_packing_rotation":
                                               ["Solution"]
                                               ["NumberOfBins"])
                 row[output_directory] = number_of_bins
-                last_rows[row_id][output_directory] += number_of_bins
-                last_row[output_directory] += number_of_bins
+                extra_rows[row_id][output_directory] += number_of_bins
+                extra_row[output_directory] += number_of_bins
 
             out_rows.append(row)
 
         for i in range(50):
-            out_rows.append(last_rows[i])
-        out_rows.append(last_row)
+            out_rows.append(extra_rows[i])
+        out_rows.append(extra_row)
 
         df = pd.DataFrame.from_records(out_rows, columns=out_fieldnames)
 
         def highlight(s):
-            return [('background-color: green'
+            return [('background-color: lightgreen'
                      if s[fieldname] == int(s[bksv_field])
-                     else 'background-color: red')
+                     else 'background-color: pink')
                     if fieldname in output_directories
                     else ''
                     for fieldname in out_fieldnames]
         df = df.style.apply(highlight, axis = 1)
-        st.dataframe(df)
+        show_datafram(df)
