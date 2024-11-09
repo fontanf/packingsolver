169a088ad347bdcf279e0a8c3148368a3f72d409
Florian Fontan <dev@florian-fontan.fr>
2024-11-09 11:23:47 +0100

    Add long2020 benchmark

--- scripts/process_benchmarks.py
+++ scripts/process_benchmarks.py
@@ -291,6 +291,68 @@ elif benchmark == "rectangleguillotine_bin_packing_3nhr":
         show_datafram(df)
 
 
+elif benchmark == "rectangleguillotine_long2020":
+
+    datacsv_path = os.path.join(
+            "data",
+            "rectangle",
+            "data_long2020.csv")
+
+    data_dir = os.path.dirname(os.path.realpath(datacsv_path))
+    with open(datacsv_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        # Get fieldnames of CSV output file.
+        out_fieldnames = reader.fieldnames
+        for output_directory in output_directories:
+            out_fieldnames.append(output_directory)
+
+        result_columns = [
+                "long2020 / SILP / Solution value",
+                "long2020 / HCG2 / Solution value"]
+
+        out_rows = []
+
+        for row in reader:
+            row[bksv_field] = int(row[bksv_field])
+
+            for output_directory in output_directories:
+                # Retrieve value.
+                json_output_path = os.path.join(
+                        benchmark_directory,
+                        output_directory,
+                        row["Path"] + "_output.json")
+                json_output_file = open(json_output_path, "r")
+                json_data = json.load(json_output_file)
+                number_of_bins = int(json_data["Output"]
+                                              ["Solution"]
+                                              ["NumberOfBins"])
+                # Update current row.
+                row[output_directory] = number_of_bins
+
+            # Update result columns of extra rows.
+            for result_column in result_columns:
+                number_of_bins = int(row[result_column])
+                row[result_column] = number_of_bins
+
+            # Add current row.
+            out_rows.append(row)
+
+        df = pd.DataFrame.from_records(out_rows, columns=out_fieldnames)
+
+        def highlight(s):
+            return [('background-color: lightgreen'
+                     if s[fieldname] == s[bksv_field]
+                     else ('background-color: pink'
+                           if s[fieldname] > s[bksv_field]
+                           else 'background-color: yellow'))
+                    if fieldname in result_columns + output_directories
+                    else ''
+                    for fieldname in out_fieldnames]
+        df = df.style.apply(highlight, axis = 1)
+        show_datafram(df)
+
+
 elif benchmark == "rectangle_bin_packing_oriented":
 
     datacsv_path = os.path.join(
