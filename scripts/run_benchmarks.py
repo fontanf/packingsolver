8f9a6e98b28bdce16767a18b2615ecce577a4cba
Florian Fontan <dev@florian-fontan.fr>
2024-11-09 11:23:47 +0100

    Add long2020 benchmark

--- scripts/run_benchmarks.py
+++ scripts/run_benchmarks.py
@@ -184,6 +184,52 @@ if __name__ == "__main__":
                 run_command(command)
 
 
+    if benchmark == "rectangleguillotine_long2020":
+
+        datacsv_path = os.path.join(
+                "data",
+                "rectangle",
+                "data_long2020.csv")
+
+        data_dir = os.path.dirname(os.path.realpath(datacsv_path))
+        with open(datacsv_path, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if not row["Path"]:
+                    break
+                if args.sub and args.sub != row["Dataset"][-1]:
+                    continue
+
+                instance_path = os.path.join(
+                        data_dir,
+                        row["Path"])
+
+                json_output_path = os.path.join(
+                        output_directory,
+                        row["Path"] + "_output.json")
+                if not os.path.exists(os.path.dirname(json_output_path)):
+                    os.makedirs(os.path.dirname(json_output_path))
+
+                certificate_path = os.path.join(
+                        output_directory,
+                        row["Path"] + "_solution.csv")
+                if not os.path.exists(os.path.dirname(certificate_path)):
+                    os.makedirs(os.path.dirname(certificate_path))
+
+                command = (
+                        rectangleguillotine_main
+                        + "  --verbosity-level 1"
+                        + "  --items \"" + instance_path + "\""
+                        + " --bin-infinite-copies"
+                        + " --objective bin-packing"
+                        + " --predefined 3NVO"
+                        + " --one2cut 1"
+                        + " --time-limit 5"
+                        + "  --output \"" + json_output_path + "\""
+                        + " --certificate \"" + certificate_path + "\"")
+                run_command(command)
+
+
     if benchmark == "rectangle_bin_packing_oriented":
 
         datacsv_path = os.path.join(
