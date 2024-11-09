caf2ece86bd13c0fc198475a46f4562b6efba9d9
Florian Fontan <dev@florian-fontan.fr>
2024-11-09 19:41:08 +0100

    Add benchmark rectangleguillotine_bin_packing_3hvo

--- scripts/process_benchmarks.py
+++ scripts/process_benchmarks.py
@@ -459,6 +459,88 @@ elif benchmark == "rectangleguillotine_bin_packing_3hao":
         show_datafram(df)
 
 
+elif benchmark == "rectangleguillotine_bin_packing_3hvo":
+
+    datacsv_path = os.path.join(
+            "data",
+            "rectangle",
+            "data_bin_packing_3hvo.csv")
+
+    data_dir = os.path.dirname(os.path.realpath(datacsv_path))
+    with open(datacsv_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        # Get fieldnames of CSV output file.
+        out_fieldnames = reader.fieldnames
+        for output_directory in output_directories:
+            out_fieldnames.append(output_directory + " / Solution value")
+
+        result_columns = [fieldname for fieldname in out_fieldnames
+                          if "Solution value" in fieldname]
+
+        out_rows = []
+
+        # Initialize extra rows.
+        extra_rows = [
+                {
+                    "Path": dataset,
+                    bksv_field: 0,
+                }
+                for dataset in ["alvarez2002", "others"]]
+        for fieldname in [bksv_field] + result_columns:
+            for row in extra_rows:
+                row[fieldname] = 0
+
+        for row in reader:
+            if not row["Path"]:
+                break
+
+            row[bksv_field] = int(row[bksv_field])
+
+            # Fill current row.
+            for output_directory in output_directories:
+                json_output_path = os.path.join(
+                        benchmark_directory,
+                        output_directory,
+                        row["Path"] + "_output.json")
+                json_output_file = open(json_output_path, "r")
+                json_data = json.load(json_output_file)
+                row[output_directory + " / Solution value"] = (
+                        json_data["Output"]["Solution"]["NumberOfBins"])
+
+            # Get extra rows to update.
+            row_id = (
+                    0 if "alvarez2002" in row["Path"] else 1)
+
+            # Update "Best known solution value" column of extra row.
+            extra_rows[row_id][bksv_field] += row[bksv_field]
+
+            # Update result columns of extra rows.
+            for result_column in result_columns:
+                number_of_bins = int(row[result_column])
+                row[result_column] = number_of_bins
+                extra_rows[row_id][result_column] += number_of_bins
+
+            # Add current row.
+            out_rows.append(row)
+
+        # Add extra rows.
+        for row in extra_rows:
+            out_rows.append(row)
+
+        df = pd.DataFrame.from_records(out_rows, columns=out_fieldnames)
+
+        def highlight(s):
+            return [('background-color: lightgreen'
+                     if s[fieldname] == s[bksv_field]
+                     else 'background-color: pink')
+                    if "Solution value" in fieldname
+                    else ''
+                    for fieldname in out_fieldnames]
+        df = df.style.apply(highlight, axis = 1)
+        show_datafram(df)
+
+
 elif benchmark == "rectangle_bin_packing_oriented":
 
     datacsv_path = os.path.join(
