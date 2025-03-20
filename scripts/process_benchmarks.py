940d97f61ed14974356ddb291837b9b53ea0e7cf
Florian Fontan <dev@florian-fontan.fr>
2025-03-20 19:01:03 +0100

    Add benchmark rectangle VSBPP oriented pisinger2005

--- scripts/process_benchmarks.py
+++ scripts/process_benchmarks.py
@@ -1748,6 +1748,98 @@ elif benchmark == "rectangle_bin_packing_rotation":
         show_datafram(df)
 
 
+elif benchmark == "rectangle_variable_sized_bin_packing_oriented_pisinger2005":
+
+    datacsv_path = os.path.join(
+            "data",
+            "rectangle",
+            "data_variable_sized_bin_packing_oriented_pisinger2005.csv")
+
+    data_dir = os.path.dirname(os.path.realpath(datacsv_path))
+    with open(datacsv_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+
+        # Get fieldnames of CSV output file.
+        out_fieldnames = reader.fieldnames
+        for output_directory in output_directories:
+            out_fieldnames.append(output_directory + " / Solution value")
+
+        result_columns = [fieldname for fieldname in out_fieldnames
+                          if "Solution value" in fieldname]
+
+        # Add gap columns.
+        out_fieldnames_tmp = []
+        for fieldname in out_fieldnames:
+            out_fieldnames_tmp.append(fieldname)
+            if "Solution value" in fieldname:
+                out_fieldnames_tmp.append(
+                        fieldname.replace("Solution value", "Gap"))
+        out_fieldnames = out_fieldnames_tmp
+
+        out_rows = []
+
+        # Initialize extra rows.
+        extra_rows = [{"Path": "Total", bksv_field: 0}]
+        for fieldname in [bksv_field] + result_columns:
+            for row in extra_rows:
+                row[fieldname] = 0
+                row[fieldname.replace("Solution value", "Gap")] = 0
+
+        for row in reader:
+
+            row[bksv_field] = float(row[bksv_field])
+
+            # Fill current row.
+            for output_directory in output_directories:
+                json_output_path = os.path.join(
+                        benchmark_directory,
+                        output_directory,
+                        row["Path"] + "_output.json")
+                json_output_file = open(json_output_path, "r")
+                json_data = json.load(json_output_file)
+                row[output_directory + " / Solution value"] = (
+                        json_data["Output"]["Solution"]["BinCost"])
+
+            # Get extra rows to update.
+            row_id = 0
+
+            # Update "Best known solution value" column of extra rows.
+            extra_rows[row_id][bksv_field] += row[bksv_field]
+
+            # Update result columns of extra rows.
+            for result_column in result_columns:
+                cost = float(row[result_column])
+                row[result_column] = cost
+                extra_rows[row_id][result_column] += cost
+
+                # Compute gap.
+                gap = (cost - row[bksv_field]) / row[bksv_field] * 100
+                gap_column = result_column.replace("Solution value", "Gap")
+                row[gap_column] = gap
+                extra_rows[row_id][gap_column] += gap
+
+            # Add current row.
+            out_rows.append(row)
+
+        # Add extra rows.
+        for row in extra_rows:
+            out_rows.append(row)
+
+        df = pd.DataFrame.from_records(out_rows, columns=out_fieldnames)
+
+        def highlight(s):
+            return [('background-color: lightgreen'
+                     if s[fieldname] == s[bksv_field]
+                     else ('background-color: pink'
+                           if s[fieldname] > s[bksv_field]
+                           else 'background-color: yellow'))
+                    if fieldname in result_columns
+                    else ''
+                    for fieldname in out_fieldnames]
+        df = df.style.apply(highlight, axis = 1)
+        show_datafram(df)
+
+
 elif benchmark == "boxstacks_knapsack_roadef2022_2024-04-25":
 
     datacsv_path = os.path.join(
