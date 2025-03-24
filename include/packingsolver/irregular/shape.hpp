363db5f3fcaa1131a0e250c3bcbf2c8877c92ace
Hugo Chiang <hugo@gyroflow.xyz>
2025-03-25 00:57:04 +0800

    update per feedback
    -  move changes into the `include/packingsolver/shape.hpp` file
    - remove the unused includes from `shape.cpp`
    - remove from `src/irregular/shape_closure.hpp` the function declarations of functions which are not called outside of `src/irregular/shape_closure.cpp`

--- include/packingsolver/irregular/shape.hpp
+++ include/packingsolver/irregular/shape.hpp
@@ -356,5 +356,24 @@ bool equal(
         const Shape& shape_1,
         const Shape& shape_2);
 
+// Check if a point is on a line segment
+bool is_point_on_line_segment(
+        const Point& p,
+        const Point& start,
+        const Point& end);
+
+// Check if a point is strictly inside a shape (excluding the boundary)
+bool is_point_strictly_inside_shape(
+        const Point& point,
+        const Shape& shape);
+
+// Check if a point is inside a shape or on its boundary
+bool is_point_inside_or_on_shape(
+        const Point& point,
+        const Shape& shape);
+
+std::vector<Shape> borders(
+        const Shape& shape);
+
 }
 }
--- src/irregular/shape_closure.hpp
+++ src/irregular/shape_closure.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "packingsolver/irregular/instance.hpp"
+#include "packingsolver/irregular/shape.hpp"
 
 namespace packingsolver
 {
@@ -8,22 +8,6 @@ namespace irregular
 {
 
 Shape close_inflated_elements(const std::vector<ShapeElement>& inflated_elements, bool is_deflating = false);
-
-LengthDbl euclidean_distance(const Point& p1, const Point& p2);
-
-bool are_line_segments_collinear(
-        const Point& line1_start, 
-        const Point& line1_end, 
-        const Point& line2_start, 
-        const Point& line2_end);
-
-bool is_line_segment_tangent_to_arc(
-        const Point& line_start, 
-        const Point& line_end, 
-        const Point& arc_center, 
-        const Point& arc_start);
-
-bool are_arcs_tangent(const ShapeElement& arc1, const ShapeElement& arc2);
 
 }
 }
