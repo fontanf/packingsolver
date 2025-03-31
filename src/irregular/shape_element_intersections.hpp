37ebfdc8ababeefa54d312fe4c294d4c2754f75e
Florian Fontan <dev@florian-fontan.fr>
2025-03-31 19:14:04 +0200

    Move compute_intersections into own file

---
+++ src/irregular/shape_element_intersections.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "packingsolver/irregular/shape.hpp"
+
+namespace packingsolver
+{
+namespace irregular
+{
+
+std::vector<Point> compute_intersections(
+        const ShapeElement& element_1,
+        const ShapeElement& element_2);
+
+}
+}
