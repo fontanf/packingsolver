7e6fa70cc24b38553a640c21002c46020c9e2348
Florian Fontan <dev@florian-fontan.fr>
2026-04-02 23:32:16 +0200

    Clean sequential feasibility algorithm

--- src/irregular/sequential_feasibility.cpp
+++ src/irregular/sequential_feasibility.cpp
@@ -22,6 +22,8 @@ SequentialFeasibilityOutput packingsolver::irregular::sequential_feasibility(
     algorithm_formatter.start();
     algorithm_formatter.print_header();
 
+    const BinType& bin_type = instance.bin_type(0);
+
     // Compute total item AABB area to derive the initial bin size.
     AreaDbl total_item_aabb_area = 0;
     for (ItemTypeId item_type_id = 0;
@@ -38,18 +40,15 @@ SequentialFeasibilityOutput packingsolver::irregular::sequential_feasibility(
     // dimension (for OpenDimensionX / OpenDimensionY).
     LengthDbl x = 0;
     LengthDbl y = 0;
-    LengthDbl x_fixed = 0;
-    LengthDbl y_fixed = 0;
     if (instance.objective() == Objective::OpenDimensionX) {
-        const BinType& bin_type = instance.bin_type(instance.bin_type_id(0));
-        y_fixed = bin_type.aabb.y_max - bin_type.aabb.y_min;
-        x = 2 * total_item_aabb_area / y_fixed;
+        y = bin_type.aabb.y_max - bin_type.aabb.y_min;
+        x = 2 * total_item_aabb_area / y;
     } else if (instance.objective() == Objective::OpenDimensionY) {
-        const BinType& bin_type = instance.bin_type(instance.bin_type_id(0));
-        x_fixed = bin_type.aabb.x_max - bin_type.aabb.x_min;
-        y = 2 * total_item_aabb_area / x_fixed;
+        x = bin_type.aabb.x_max - bin_type.aabb.x_min;
+        y = 2 * total_item_aabb_area / x;
     } else {  // OpenDimensionXY
         x = std::sqrt(total_item_aabb_area / instance.parameters().open_dimension_xy_aspect_ratio);
+        y = x * instance.parameters().open_dimension_xy_aspect_ratio;
     }
 
     for (Counter it = 0;; ++it) {
@@ -61,21 +60,12 @@ SequentialFeasibilityOutput packingsolver::irregular::sequential_feasibility(
         // Build a BinPacking sub-instance with a single bin whose shape is the
         // intersection of the original bin with a rectangle restricted to the
         // current open dimension estimate.
-        const BinType& original_bin_type = instance.bin_type(instance.bin_type_id(0));
-        AxisAlignedBoundingBox restricting_aabb = original_bin_type.aabb;
-        if (instance.objective() == Objective::OpenDimensionX) {
-            restricting_aabb.x_max = x;
-        } else if (instance.objective() == Objective::OpenDimensionY) {
-            restricting_aabb.y_max = y;
-        } else {  // OpenDimensionXY
-            restricting_aabb.x_max = restricting_aabb.x_min + x;
-            restricting_aabb.y_max = restricting_aabb.y_min
-                + x * instance.parameters().open_dimension_xy_aspect_ratio;
-        }
+        AxisAlignedBoundingBox restricting_aabb = bin_type.aabb;
+        restricting_aabb.x_max = restricting_aabb.x_min + x;
+        restricting_aabb.y_max = restricting_aabb.y_min + y;
         const Shape restricting_rect = shape::build_rectangle(restricting_aabb);
         const std::vector<shape::ShapeWithHoles> intersection = shape::compute_intersection(
-                {{original_bin_type.shape_orig},
-                {restricting_rect}});
+                {{bin_type.shape_orig}, {restricting_rect}});
 
         InstanceBuilder sub_instance_builder;
         sub_instance_builder.set_objective(Objective::BinPacking);
@@ -123,16 +113,13 @@ SequentialFeasibilityOutput packingsolver::irregular::sequential_feasibility(
 
         // Shrink the open dimension for the next iteration.
         if (instance.objective() == Objective::OpenDimensionX) {
-            const BinType& bin_type = instance.bin_type(instance.bin_type_id(0));
-            x = 0.99 * solution.x_max() + bin_type.item_bin_minimum_spacing;
+            x = 0.99 * (solution.x_max() - bin_type.aabb.x_min) + bin_type.item_bin_minimum_spacing;
         } else if (instance.objective() == Objective::OpenDimensionY) {
-            const BinType& bin_type = instance.bin_type(instance.bin_type_id(0));
-            y = 0.99 * solution.y_max() + bin_type.item_bin_minimum_spacing;
+            y = 0.99 * (solution.y_max() - bin_type.aabb.y_min) + bin_type.item_bin_minimum_spacing;
         } else {  // OpenDimensionXY
-            const BinType& bin_type = instance.bin_type(instance.bin_type_id(0));
             x = 0.99 * (std::max)(
-                    solution.x_max() - solution.x_min(),
-                    solution.y_max() - solution.y_min())
+                    solution.x_max() - bin_type.aabb.x_min,
+                    solution.y_max() - bin_type.aabb.y_min)
                 + bin_type.item_bin_minimum_spacing;
             AreaDbl a_cur = (solution.x_max() - solution.x_min()) * (solution.y_max() - solution.y_min());
             LengthDbl x_cur = std::sqrt(a_cur / instance.parameters().open_dimension_xy_aspect_ratio);
